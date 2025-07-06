5605aa5389595ebe58bd8a4d60313dfdf1894061
Nikhil Rao <1139nikhilraojnv@gmail.com>
2025-07-06 11:38:15 +0530

    nultiplt commit

--- 04GIT/day02/first1.js
+++ 04GIT/day02/first1.js
@@ -2,3 +2,15 @@ const x=1;
 console.log("hlo");
 const r=4;
 const e=3;
+let fun=function(x, y){
+    console.log(x+y);
+    return x+y;
+}
+fun(4,5);
+
+let fun2=function(a,b){
+    console.log(x*y);
+    return x*y;
+
+}
+funn(3,4);
